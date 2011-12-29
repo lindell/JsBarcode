c95a249d403a6d9efcd896145d57779d1edc9756
Johan Lindell <lindell92@gmail.com>
2011-12-29 18:49:43 +0100

    Added optional arguments

--- barcode.js
+++ barcode.js
@@ -1,8 +1,26 @@
-function drawBinary(binaryString, context, height, width){
+function drawBinary(binaryString, context, options){
+
+	var default_arg = {
+		width:2,
+		height:100,
+		x:0,
+		y:0,
+		quietZone:10,
+	}
+	
+	if(options == undefined){
+		options = {};
+	}
+	
+	for(var i in default_arg) {
+		if(typeof options[i] == "undefined") options[i] = default_arg[i];
+	}
+	
+	
 	for(var i=0;i<binaryString.length;i++){
 	
-		var x = i*width;
-		var y = 0;
+		var x = i*options.width+options.x+options.quietZone;
+		var y = options.y;
 		
 		if(binaryString[i] == "1"){
 			context.fillStyle = "#000";
@@ -14,6 +32,6 @@ function drawBinary(binaryString, context, height, width){
 			console.log("Not a binary number!");
 		}
 		
-		context.fillRect(x,y,width,height);
+		context.fillRect(x,y,options.width,options.height);
 	}
 }
