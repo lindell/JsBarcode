c023ec401ae2fb1eca4753ef18f324e4666c3ebe
Johan Lindell <johan@lindell.me>
2016-03-16 18:27:15 +0100

    A big update to the barcode generator. * New options * Much more consistent and simplified layout (in regards to html and css)

--- javascripts/script.js
+++ javascripts/script.js
@@ -36,6 +36,14 @@ $(document).ready(function(){
       $(".display-text").removeClass("btn-primary");
       $(this).addClass("btn-primary");
       displayValue = $(this).val() == "true";
+
+      if(displayValue){
+        $("#font-options").slideDown("fast");
+      }
+      else{
+        $("#font-options").slideUp("fast");
+      }
+
       newBarcode();
     });
 
@@ -52,23 +60,32 @@ $(document).ready(function(){
     });
 
     $('input.color').colorPicker({
-      actionCallback: newBarcode
+      actionCallback: newBarcode,
+      init: function(elm, colors) {
+        elm.style.backgroundColor = elm.value;
+        elm.style.color = colors.rgbaMixCustom.luminance > 0.22 ? '#222' : '#ddd';
+      }
+    });
+
+    $(".dropdown-menu li a").click(function(){
+      var selText = $(this).text();
+      $(this).parents('.btn-group').find('.btn').html(selText + ' <span class="caret"></span>');
     });
 });
 
 var newBarcode = function() {
     //Convert to boolean
-    console.log(textAlign);
     $("#barcode").JsBarcode(
         $("#userInput").val(),
         {
             "format": $("#barcodeType").val(),
-            "backgroundColor": $("#background-color").val(),
+            "background": $("#background-color").val(),
             "lineColor": $("#line-color").val(),
             "fontSize": parseInt($("#bar-fontSize").val()),
             "height": parseInt($("#bar-height").val()),
             "width": $("#bar-width").val(),
-            "quite": parseInt($("#bar-margin").val()),
+            "margin": parseInt($("#bar-margin").val()),
+            "textMargin": parseInt($("#bar-text-margin").val()),
             "displayValue": displayValue,
             "textAlign": textAlign
         },
@@ -85,4 +102,5 @@ var newBarcode = function() {
     $("#bar-height-display").text($("#bar-height").val());
     $("#bar-fontSize-display").text($("#bar-fontSize").val());
     $("#bar-margin-display").text($("#bar-margin").val());
+    $("#bar-text-margin-display").text($("#bar-text-margin").val());
 };
