64aa77ec761d84e4a3450f7683a3255c038dac69
Johan Lindell <johan@lindell.me>
2016-03-23 19:21:50 +0100

    CODE128 recursive cleanup

--- barcodes/CODE128.js
+++ barcodes/CODE128.js
@@ -11,7 +11,27 @@ function CODE128(string, code) {
 
 	//The public encoding function
 	this.encoded = function() {
-		return calculate["code128" + code](string);
+		var encodingResult;
+		var startCode;
+		if(code === "B"){
+			encodingResult = nextBChar(this.string, 1);
+			startCode = 104;
+		}
+		else if(code === "C"){
+			encodingResult = nextCChar(this.string, 1);
+			startCode = 105;
+		}
+
+		return (
+			//Add the start bits
+			getEncoding(startCode) +
+			//Add the encoded bits
+			encodingResult.result +
+			//Add the checksum
+			getEncoding((encodingResult.checksum + startCode) % 103) +
+			//Add the end bits
+			getEncoding(106)
+		);
 	}
 
 	//Data for each character, the last characters will not be encoded but are used for error correction
@@ -26,33 +46,16 @@ function CODE128(string, code) {
 			158,  68,  62, 424, 412, 232, 218,  76,  74, 554, 616,
 			978, 556, 146, 340, 212, 182, 508, 268, 266, 956, 940,
 			938, 758, 782, 974, 400, 310, 118, 512, 506, 960, 954,
-			502, 518, 886, 966, /* Start codes */   668, 680, 692
+			502, 518, 886, 966, /* Start codes */   668, 680, 692,
+			5379
 		];
 	var getEncoding = function(n) {
 		return (code128b[n] ? (code128b[n] + 1000).toString(2) : '');
 	};
-	//The end bits
-	var endBin = "1100011101011";
 
 	//Use the regexp variable for validation
 	function valid() {
 		return !(this.string.search(/^[!-~ ]+$/) === -1);
-	}
-
-	//The encoder function that return a complete binary string. Data need to be validated before sent to this function
-	//This is general calculate function, which is called by code specific calculate functions
-	function calculateCode128(string, encodeFn, startCode) {
-		var encodingResult = encodeFn(string, startCode);
-		return (
-			//Add the start bits
-			getEncoding(startCode) +
-			//Add the encoded bits
-			encodingResult.result +
-			//Add the checksum
-			getEncoding((encodingResult.checksum + startCode)%103) +
-			//Add the end bits
-			endBin
-		);
 	}
 
 	function nextBChar(string, depth){
@@ -63,7 +66,6 @@ function CODE128(string, code) {
 		var index = string.charCodeAt(0) - 32;
 		var enc = getEncoding(index);
 		var weight = index * depth;
-		console.log(string[0] + "# " + index + "*" + depth + "=" + weight);
 
 		var next = nextBChar(string.substring(1), depth + 1);
 
@@ -71,7 +73,7 @@ function CODE128(string, code) {
 	}
 
 	function nextCChar(string, depth){
-		if(string.length == 0){
+		if(string.length <= 0){
 			return {"result": "", "checksum": 0};
 		}
 
@@ -83,34 +85,8 @@ function CODE128(string, code) {
 
 		return {"result": enc + next.result, "checksum": weight + next.checksum}
 	}
-
-	//Code specific calculate functions
-	var calculate = {
-		code128B: function(string) {
-			return calculateCode128(string, encodeB, 104);
-		},
-		code128C: function(string) {
-			string = string.replace(/ /g, '');
-			return calculateCode128(string, encodeC, 105);
-		}
-	}
-
-	//Encode the characters and calculate the checksum (128 B)
-	function encodeB(string, startCode) {
-		return nextBChar(string, 1);
-	}
-
-	//Encode the characters and calculate the checksum (128 C)
-	function encodeC(string, startCode) {
-		return nextCChar(string, 1);
-	}
-
-	//Get the encoded data by the id of the character
-	function encodingById(id) {
-		return getCode128b(id);
-	}
-
 }
+
 
 function CODE128B(string) {
 	return new CODE128(string, "B");
