d84e69c672b2bda57c15e661ad81146de3e5694a
Johan Lindell <johan@lindell.me>
2016-03-30 10:47:42 +0200

    Added support for text above the barcodes

--- JsBarcode.js
+++ JsBarcode.js
@@ -120,11 +120,20 @@
 		}
 
 		// Creates the barcode out of the encoded binary
+		var yFrom, yHeight;
+		if(options.textPosition == "top"){
+			yFrom = options.marginTop + options.fontSize + options.textMargin;
+		}
+		else{
+			yFrom = options.marginTop;
+		}
+		yHeight = options.height;
+
 		ctx.fillStyle = options.lineColor;
 		for(var i=0;i<binary.length;i++){
 			var x = i*options.width + options.marginLeft + barcodePadding;
 			if(binary[i] == "1"){
-				ctx.fillRect(x, options.marginTop, options.width, options.height);
+				ctx.fillRect(x, yFrom, options.width, options.height);
 			}
 		}
 
@@ -132,11 +141,16 @@
 		if(options.displayValue){
 			var x, y;
 
-			y = options.height + options.textMargin + options.marginTop;
+			if(options.textPosition == "top"){
+				y = options.marginTop + options.fontSize;
+				ctx.textBaseline = "bottom";
+			}
+			else{
+				y = options.height + options.textMargin + options.marginTop;
+				ctx.textBaseline = "top";
+			}
 
 			ctx.font = font;
-			ctx.textBaseline = "bottom";
-			ctx.textBaseline = 'top';
 
 			// Draw the text in the correct X depending on the textAlign option
 			if(options.textAlign == "left" || barcodePadding > 0){
@@ -265,6 +279,7 @@
 		fontOptions: "",
 		font: "monospace",
 		textAlign: "center",
+		textPosition: "bottom",
 		textMargin: 2,
 		fontSize: 20,
 		background: "#ffffff",
