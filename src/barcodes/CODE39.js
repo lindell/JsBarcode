7352e05a5a41b462c79263c0f0c6a2772cf18e9e
Johan Lindell <johan@lindell.me>
2016-04-13 10:03:07 +0200

    Made CODE39 into ES6

--- src/barcodes/CODE39.js
+++ src/barcodes/CODE39.js
@@ -1,34 +1,36 @@
-function CODE39(string){
-	string = string.toUpperCase();
+class CODE39 {
+	constructor(string){
+		this.string = string.toUpperCase();
 
-	var encodings = {
-		"0": 20957, "1": 29783, "2": 23639, "3": 30485,
-		"4": 20951, "5": 29813, "6": 23669, "7": 20855,
-		"8": 29789, "9": 23645, "A": 29975, "B": 23831,
-		"C": 30533, "D": 22295, "E": 30149, "F": 24005,
-		"G": 21623, "H": 29981, "I": 23837, "J": 22301,
-		"K": 30023, "L": 23879, "M": 30545, "N": 22343,
-		"O": 30161, "P": 24017, "Q": 21959, "R": 30065,
-		"S": 23921, "T": 22385, "U": 29015, "V": 18263,
-		"W": 29141, "X": 17879, "Y": 29045, "Z": 18293,
-		"-": 17783, ".": 29021, " ": 18269, "$": 17477,
-		"/": 17489, "+": 17681, "%": 20753, "*": 35770
+		this.encodings = {
+			"0": 20957, "1": 29783, "2": 23639, "3": 30485,
+			"4": 20951, "5": 29813, "6": 23669, "7": 20855,
+			"8": 29789, "9": 23645, "A": 29975, "B": 23831,
+			"C": 30533, "D": 22295, "E": 30149, "F": 24005,
+			"G": 21623, "H": 29981, "I": 23837, "J": 22301,
+			"K": 30023, "L": 23879, "M": 30545, "N": 22343,
+			"O": 30161, "P": 24017, "Q": 21959, "R": 30065,
+			"S": 23921, "T": 22385, "U": 29015, "V": 18263,
+			"W": 29141, "X": 17879, "Y": 29045, "Z": 18293,
+			"-": 17783, ".": 29021, " ": 18269, "$": 17477,
+			"/": 17489, "+": 17681, "%": 20753, "*": 35770
+		};
 	};
 
-	this.encode = function(){
+	encode(){
 		var result = "";
-		result += encodings["*"].toString(2);
-		for(var i=0; i<string.length; i++){
-			result += encodings[string[i]].toString(2) + "0";
+		result += this.encodings["*"].toString(2);
+		for(var i=0; i<this.string.length; i++){
+			result += this.encodings[this.string[i]].toString(2) + "0";
 		}
-		result += encodings["*"].toString(2);
+		result += this.encodings["*"].toString(2);
 
-		return {data: result, text: string};
+		return {data: result, text: this.string};
 	};
 
 	//Use the regexp variable for validation
-	this.valid = function(){
-		return string.search(/^[0-9A-Z\-\.\ \$\/\+\%]+$/) !== -1;
+	valid(){
+		return this.string.search(/^[0-9A-Z\-\.\ \$\/\+\%]+$/) !== -1;
 	};
 }
 
