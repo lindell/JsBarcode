19afc070ae12ff7724df2f7e2c64ddc20b7e2fe2
Johan Lindell <johan@lindell.me>
2016-06-26 16:13:58 +0200

    Included defaults file

---
+++ src/defaults/defaults.js
@@ -0,0 +1,22 @@
+var defaults = {
+	width: 2,
+	height: 100,
+	format: "auto",
+	displayValue: true,
+	fontOptions: "",
+	font: "monospace",
+	textAlign: "center",
+	textPosition: "bottom",
+	textMargin: 2,
+	fontSize: 20,
+	background: "#ffffff",
+	lineColor: "#000000",
+	margin: 10,
+	marginTop: undefined,
+	marginBottom: undefined,
+	marginLeft: undefined,
+	marginRight: undefined,
+	valid: function(valid){}
+};
+
+export default defaults;
