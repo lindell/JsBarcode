1fc68d46b9c1c74fa1ee87e6b7d1158ad3a57e3d
Johan Lindell <johan@lindell.me>
2016-06-27 20:14:06 +0200

    Moved functionality out of the getRenderProperties function

--- src/help/getRenderProperties.js
+++ src/help/getRenderProperties.js
@@ -18,21 +18,11 @@ import getOptionsFromElement from "./getOptionsFromElement.js";
 function getRenderProperties(element){
 	// If the element is a string, query select call again
 	if(typeof element === "string"){
-		var selector = document.querySelectorAll(element);
-		if(selector.length === 0){
-			throw new Error("No element found");
-		}
-		else{
-			let returnArray = [];
-			for(let i = 0; i < selector.length; i++){
-				returnArray.push(getRenderProperties(selector[i]));
-			}
-			return returnArray;
-		}
+		return querySelectedRenderProperties(element);
 	}
 	// If element is array. Recursivly call with every object in the array
 	else if(Array.isArray(element)){
-		let returnArray = [];
+		var returnArray = [];
 		for(let i = 0; i < element.length; i++){
 			returnArray.push(getRenderProperties(element[i]));
 		}
@@ -40,15 +30,7 @@ function getRenderProperties(element){
 	}
 	// If element, render on canvas and set the uri as src
 	else if(typeof HTMLCanvasElement !== 'undefined' && element instanceof HTMLImageElement){
-		var canvas = document.createElement('canvas');
-		return {
-			element: canvas,
-			options: getOptionsFromElement(element),
-			renderer: "canvas",
-			afterRender: function(){
-				element.setAttribute("src", canvas.toDataURL());
-			}
-		};
+		return newCanvasRenderProperties(element);
 	}
 	// If SVG
 	else if(typeof SVGElement !== 'undefined' && element instanceof SVGElement){
@@ -76,6 +58,34 @@ function getRenderProperties(element){
 	else{
 		throw new Error("Not supported type to render on.");
 	}
+}
+
+
+function querySelectedRenderProperties(string){
+	var selector = document.querySelectorAll(string);
+	if(selector.length === 0){
+		throw new Error("No element found");
+	}
+	else{
+		let returnArray = [];
+		for(let i = 0; i < selector.length; i++){
+			returnArray.push(getRenderProperties(selector[i]));
+		}
+		return returnArray;
+	}
+}
+
+
+function newCanvasRenderProperties(imgElement){
+	var canvas = document.createElement('canvas');
+	return {
+		element: canvas,
+		options: getOptionsFromElement(imgElement),
+		renderer: "canvas",
+		afterRender: function(){
+			imgElement.setAttribute("src", canvas.toDataURL());
+		}
+	};
 }
 
 export default getRenderProperties;
