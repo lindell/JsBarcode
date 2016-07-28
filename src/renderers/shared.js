9d0be6a468b13ea057f44b8bc8dad688d72f5b30
Johan Lindell <johan@lindell.me>
2016-07-28 11:13:49 +0200

    Fixed textMargin when displayValue is set to false

--- src/renderers/shared.js
+++ src/renderers/shared.js
@@ -2,8 +2,7 @@ import merge from "../help/merge.js";
 
 function getEncodingHeight(encoding, options){
 	return options.height +
-		((options.displayValue && encoding.text.length > 0) ? options.fontSize : 0) +
-		options.textMargin +
+		((options.displayValue && encoding.text.length > 0) ? options.fontSize + options.textMargin : 0) +
 		options.marginTop +
 		options.marginBottom;
 }
