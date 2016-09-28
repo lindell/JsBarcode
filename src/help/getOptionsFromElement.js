89be5f81dc507215502c8d677603870d2ea5452b
Johan Lindell <johan@lindell.me>
2016-09-28 09:15:08 +0200

    Now uses dash to convert camel case properties in HTML elements

--- src/help/getOptionsFromElement.js
+++ src/help/getOptionsFromElement.js
@@ -8,9 +8,12 @@ function getOptionsFromElement(element){
 		var name = attributes[i].name;
 		var value = attributes[i].value;
 
-		var match = name.match(/(jsbarcode|data)-([A-Za-z0-9]+)/i);
+		var match = name.match(/(jsbarcode|data)-([A-Za-z0-9-]+)/i);
 		if(match){
 			var property = match[2];
+
+			// Transforms foo-bar to fooBar
+			property = property.replace(/-[a-zA-Z]/, (val) => val[1].toUpperCase());
 
 			options[property] = value;
 		}
