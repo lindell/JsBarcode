f3ee862e56e20a37bcd08f223bd10ce2f2a370f7
Johan Lindell <johan@lindell.me>
2017-05-11 21:07:33 +0200

    Changed xml test to use javascript that works for older node versions

--- test/node/nodesvg.test.js
+++ test/node/nodesvg.test.js
@@ -1,18 +1,20 @@
 var assert = require('assert');
 var JsBarcode = require('../../bin/JsBarcode.js');
-const { DOMImplementation, XMLSerializer } = require('xmldom');
-const xmlSerializer = new XMLSerializer();
-const document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+var xmldom  = require('xmldom');
+var DOMImplementation = xmldom.DOMImplementation;
+var XMLSerializer = xmldom.XMLSerializer;
+var xmlSerializer = new XMLSerializer();
+var document = new DOMImplementation().createDocument('http://www.w3.org/1999/xhtml', 'html', null);
 
 describe('SVG', function() {
   it('should work with external SVG implementation', function () {
-    const svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    var svgNode = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
     JsBarcode(svgNode, 'test', {
       xmlDocument: document
     });
 
-    let xml = xmlSerializer.serializeToString(svgNode);
+    var xml = xmlSerializer.serializeToString(svgNode);
     assert(xml.length > 200);
   });
 });
