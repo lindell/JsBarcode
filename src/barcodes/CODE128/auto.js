fbf4ea417761f248e3425430c3f13fe676142967
Sanich <sancho@cdrpro.ru>
2017-05-12 23:29:54 +0300

    Added comments and renamed several functions (CODE128)

--- src/barcodes/CODE128/auto.js
+++ src/barcodes/CODE128/auto.js
@@ -1,17 +1,19 @@
 import { A_START_CHAR, B_START_CHAR, C_START_CHAR, A_CHARS, B_CHARS, C_CHARS } from './constants';
 
-const getALength = (string) => string.match(new RegExp(`^${A_CHARS}*`))[0].length;
-const getBLength = (string) => string.match(new RegExp(`^${B_CHARS}*`))[0].length;
-const matchC = (string) => string.match(new RegExp(`^${C_CHARS}*`))[0];
+// Match Set functions
+const matchSetALength = (string) => string.match(new RegExp(`^${A_CHARS}*`))[0].length;
+const matchSetBLength = (string) => string.match(new RegExp(`^${B_CHARS}*`))[0].length;
+const matchSetC = (string) => string.match(new RegExp(`^${C_CHARS}*`))[0];
 
-function fromAB(string, isA){
+// CODE128A or CODE128B
+function autoSelectFromAB(string, isA){
 	const ranges = isA ? A_CHARS : B_CHARS;
 	const untilC = string.match(new RegExp(`^(${ranges}+?)(([0-9]{2}){2,})([^0-9]|$)`));
 
 	if (untilC) return (
 		untilC[1] +
 		String.fromCharCode(204) +
-		fromC(string.substring(untilC[1].length))
+		autoSelectFromC(string.substring(untilC[1].length))
 	);
 
 	const chars = string.match(new RegExp(`^${ranges}+`))[0];
@@ -20,34 +22,39 @@ function fromAB(string, isA){
 	return (
 		chars +
 		String.fromCharCode(isA ? 205 : 206) +
-		fromAB(string.substring(chars.length), !isA)
+		autoSelectFromAB(string.substring(chars.length), !isA)
 	);
 }
 
-function fromC(string) {
-	const cMatch = matchC(string);
+// CODE128C
+function autoSelectFromC(string) {
+	const cMatch = matchSetC(string);
 	const length = cMatch.length;
 
 	if (length === string.length) return string;
 	string = string.substring(length);
 
-	const isA = getALength(string) >= getBLength(string);
-	return cMatch + String.fromCharCode(isA ? 206 : 205) + fromAB(string, isA);
+	// Select A/B depending on the longest match
+	const isA = matchSetALength(string) >= matchSetBLength(string);
+	return cMatch + String.fromCharCode(isA ? 206 : 205) + autoSelectFromAB(string, isA);
 }
 
+// Detect Code Set (A, B or C) and format the string
 export default (string) => {
 	let newString;
-	const cLength = matchC(string).length;
+	const cLength = matchSetC(string).length;
 
+	// Select 128C if the string start with enough digits
 	if (cLength >= 2) {
-		newString = C_START_CHAR + fromC(string);
+		newString = C_START_CHAR + autoSelectFromC(string);
 	} else {
-		const isA = getALength(string) > getBLength(string);
-		newString = (isA ? A_START_CHAR : B_START_CHAR) + fromAB(string, isA);
+		// Select A/B depending on the longest match
+		const isA = matchSetALength(string) > matchSetBLength(string);
+		newString = (isA ? A_START_CHAR : B_START_CHAR) + autoSelectFromAB(string, isA);
 	}
 
 	return newString.replace(
-		/[\xCD\xCE]([^])[\xCD\xCE]/,
+		/[\xCD\xCE]([^])[\xCD\xCE]/, // Any sequence between 205 and 206 characters
 		(match, char) => String.fromCharCode(203) + char
 	);
 };
--- src/barcodes/CODE128/constants.js
+++ src/barcodes/CODE128/constants.js
@@ -6,6 +6,14 @@ export const A_START_CHAR = String.fromCharCode(208); // StartCodeA + 105
 export const B_START_CHAR = String.fromCharCode(209); // StartCodeB + 105
 export const C_START_CHAR = String.fromCharCode(210); // StartCodeC + 105
 
-export const A_CHARS = "[\x00-\x5F\xC8-\xCF]"; // ASCII ranges 0-95 and 200-207 (FUNCs and SHIFTs)
-export const B_CHARS = "[\x20-\x7F\xC8-\xCF]"; // ASCII ranges 32-127 and 200-207 (FUNCs and SHIFTs)
-export const C_CHARS = "(\xCF*[0-9]{2}\xCF*)"; // Number pairs or [FNC1]
+// 128A (Code Set A)
+// ASCII characters 00 to 95 (0–9, A–Z and control codes), special characters, and FNC 1–4
+export const A_CHARS = "[\x00-\x5F\xC8-\xCF]";
+
+// 128B (Code Set B)
+// ASCII characters 32 to 127 (0–9, A–Z, a–z), special characters, and FNC 1–4
+export const B_CHARS = "[\x20-\x7F\xC8-\xCF]";
+
+// 128C (Code Set C)
+// 00–99 (encodes two digits with a single code point) and FNC1
+export const C_CHARS = "(\xCF*[0-9]{2}\xCF*)";
