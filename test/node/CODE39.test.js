4114e72d399bf349a6fa3454f6bb56e31a1f8a48
Johan Lindell <johan@lindell.me>
2019-01-24 21:53:19 +0100

    Fixed codabar and code39 tests

--- test/node/CODE39.test.js
+++ test/node/CODE39.test.js
@@ -1,31 +1,26 @@
 const assert = require('assert');
-const CODE39 = require('../../lib/barcodes/CODE39').default;
+const {encode, valid} = require('../../lib/barcodes/CODE39').default;
 
 describe('CODE39', function() {
   it('should be able to encode normal text', function () {
-    const enc = new CODE39("AB12", {});
     assert.equal("100010111011101011101010001011101011101000101110111010001010111010111000101011101000101110111010"
-      , enc.encode().data);
+      , encode('AB12', {}).data);
   });
 
   it('should warn with invalid text', function () {
-    const enc = new CODE39("AB!12", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("AB!12", {}));
   });
 
   it('should make lowercase to uppercase', function () {
-    const enc = new CODE39("abc123ABC", {});
-    assert.equal("ABC123ABC", enc.encode().text);
+    assert.equal("ABC123ABC", encode('abc123ABC', {}).text);
   });
 
   it('should calculate correct checksums', function () {
-    const enc = new CODE39("ABCDEFG", {mod43: true});
     assert.equal("1000101110111010111010100010111010111010001011101110111010001010101011100010111011101011100010101011101110001010101010001110111011101000111010101000101110111010"
-      , enc.encode().data);
+      , encode('ABCDEFG', {mod43: true}).data);
   });
 
   it('should work with text option', function () {
-    const enc = new CODE39("AB12", {text: "THISISTEXT"});
-    assert.equal("THISISTEXT", enc.encode().text);
+    assert.equal("THISISTEXT", encode("AB12", {text: "THISISTEXT"}).text);
   });
 });
--- test/node/codabar.test.js
+++ test/node/codabar.test.js
@@ -1,68 +1,56 @@
 const assert = require('assert');
-const Codabar = require('../../lib/barcodes/codabar').default;
+const {encode, valid} = require('../../lib/barcodes/codabar').default;
 
 describe('Codabar', function() {
   it('should encode a string with start and stop characters', function() {
-    const enc = new Codabar("A12345B", {});
     assert.equal("10110010010101011001010100101101100101010101101001011010100101001001011"
-      , enc.encode().data);
+      , encode("A12345B", {}).data);
   });
 
   it('should add start and stop characters to a string without them', function() {
-    const enc = new Codabar("12345", {});
     // should encode to "A12345A"
     assert.equal("10110010010101011001010100101101100101010101101001011010100101011001001"
-      , enc.encode().data);
+      , encode("12345", {}).data);
   });
 
   it('should return text string without start/stop characters', function() {
-    const enc = new Codabar("A12345B", {});
-    assert.equal("12345", enc.encode().text)
+    assert.equal("12345", encode("A12345B", {}).text)
   });
 
   it('should warn with invalid start/stop characters', function () {
-    const enc = new Codabar("X12345Y", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("X12345Y"));
   });
 
   it('should warn with only a start character', function () {
-    const enc = new Codabar("A12345", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("A12345"));
   });
 
   it('should warn with only an invalid start character', function () {
-    const enc = new Codabar("X12345", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("X12345"));
   });
 
   it('should warn with only a stop character', function () {
-    const enc = new Codabar("12345A", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("12345A"));
   });
 
   it('should warn with only an invalid stop character', function () {
-    const enc = new Codabar("12345X", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("12345X"));
   });
 
   it('should warn with only start and stop characters', function() {
-    const enc = new Codabar("AA", {})
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("AA"));
   });
 
   it('should warn with an empty string', function() {
-    const enc = new Codabar("", {})
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid(""));
   });
 
   it('should warn with invalid input', function () {
-    const enc = new Codabar("A1234OOPS56A", {});
-    assert.equal(false, enc.valid());
+    assert.equal(false, valid("A1234OOPS56A"));
   });
 
   it('should work with text option', function () {
-    const enc = new Codabar("A1234OOPS56A", {text: "THISISATEXT"});
-    assert.equal("THISISATEXT", enc.encode().text);
+    assert.equal("THISISATEXT", encode("A1234OOPS56A", {text: "THISISATEXT"}).text);
   });
 
 });
