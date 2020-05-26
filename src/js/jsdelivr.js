3f34375ace432eaa7196f5812176319e42175de2
Johan Lindell <johan@lindell.me>
2020-05-26 07:44:58 +0200

    Fixed jsdelivr links to use the new npm hosting

--- src/js/jsdelivr.js
+++ src/js/jsdelivr.js
@@ -7,13 +7,13 @@ window.addEventListener("load", function load(event){
     if (jsdelivrRequest.readyState === XMLHttpRequest.DONE) {
       if (jsdelivrRequest.status === 200) {
         var json = JSON.parse(jsdelivrRequest.responseText);
-        var version = json[0]['lastversion'];
+        var version = json['tags']['latest'];
         updateVersion(version);
       }
     }
   };
 
-  jsdelivrRequest.open('GET', 'https://api.jsdelivr.com/v1/jsdelivr/libraries?name=jsbarcode', true);
+  jsdelivrRequest.open('GET', 'https://data.jsdelivr.com/v1/package/npm/jsbarcode', true);
   jsdelivrRequest.send(null);
 });
 
@@ -23,7 +23,7 @@ function updateVersion(version) {
   downloadLinks.forEach(function(downloadLink) {
     var filename = downloadLink.getAttribute('filename');
     var path = downloadLink.getAttribute('path');
-    var url = "https://cdn.jsdelivr.net/jsbarcode/" + version + path + filename;
+    var url = "https://cdn.jsdelivr.net/npm/jsbarcode@"+ version + "/dist" + path + filename;
     downloadLink.setAttribute('href', url);
   });
 }
