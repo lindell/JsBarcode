fd1347f27aa45b3d2fa3be00bc68e87f9af0e5d5
Johan Lindell <johan@lindell.me>
2025-06-10 07:32:14 +0200

    Restored barcodes/index.js

--- src/barcodes/index.js
+++ src/barcodes/index.js
@@ -1,2 +1,22 @@
-import {CODE39} from './CODE39'
-export default {CODE39}
+import {CODE39} from './CODE39/';
+import {CODE128, CODE128A, CODE128B, CODE128C} from './CODE128/';
+import {EAN13, EAN8, EAN5, EAN2, UPC, UPCE} from './EAN_UPC/';
+import {ITF, ITF14} from './ITF/';
+import {MSI, MSI10, MSI11, MSI1010, MSI1110} from './MSI/';
+import {pharmacode} from './pharmacode/';
+import {codabar} from './codabar';
+import {CODE93, CODE93FullASCII} from './CODE93/';
+import {GenericBarcode} from './GenericBarcode/';
+
+export default {
+	CODE39,
+	CODE128, CODE128A, CODE128B, CODE128C,
+	EAN13, EAN8, EAN5, EAN2, UPC, UPCE,
+	ITF14,
+	ITF,
+	MSI, MSI10, MSI11, MSI1010, MSI1110,
+	pharmacode,
+	codabar,
+	CODE93, CODE93FullASCII,
+	GenericBarcode
+};
